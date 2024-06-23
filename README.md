e29d961f64d0296eba49666f56f42e11e7de2195
fmann <fmannanov01@gmail.com>
2024-06-23 15:26:55 +0300

    Bug FIX

--- README.md
+++ README.md
@@ -7,15 +7,19 @@
   - FastAPI, zeep.
 
 ## Запуск:
-- Переходим в папку src 
-    - cd ./src
-- Создаём и заполняем .env файл по примеру в .env.example
-- Ставим виртуальное окружение 
-    - python -m venv venv
-- Устанавливаем зависимости 
-    - pip install -r requirements.txt
-- Запускаем приложение 
-    - python main.py
+  - Находясь в корне проекта, в папке soap_api
+  - Создаём и заполняем .env файл по примеру в .env.example
+  - Ставим виртуальное окружение 
+      - python -m venv venv
+  - Устанавливаем зависимости 
+      - pip install -r requirements.txt
+  - Переходим в папку src 
+      - cd ./src
+  - Запускаем приложение 
+      - python main.py
 
+## Формат csv файлов:
+  При добавлении и изменении доменов: name, price, min_price, fix_price, currency, category
+  При получении и удалении доменов: name
 ## Дополнительно:
-- http://127.0.0.1:8000/api/v1/docs/ - Swagger sedo_api
+- http://127.0.0.1:8000/api/v1/docs/ - Документация и интерфейс sedo_api
--- src/api/v1/domain.py
+++ src/api/v1/domain.py
@@ -35,6 +35,18 @@ async def add_domain_async(client, params, chunk_counter, chunk_len):
         return {'error': 'Ошибка при добавлении данных'}
 
 
+async def update_domain_async(client, params, chunk_counter, chunk_len):
+    try:
+        result = client.service.DomainEdit(member=params)
+        logger.info(f"Чанк {chunk_counter} из {chunk_len}")
+        logger.info('Результат:')
+        logger.info(result)
+        return result
+    except Exception as er:
+        logger.info(er)
+        return {'error': 'Ошибка при изменении данных'}
+
+
 async def delete_domain_async(client, params, chunk_counter, chunk_len):
     try:
         result = client.service.DomainDelete(member=params)
@@ -61,7 +73,8 @@ async def get_status_domain_async(client, params, chunk_counter, chunk_len):
 
 @router.post(
     '/add_domains',
-    summary="Add domains from CSV file"
+    summary="Add domains from CSV file",
+    description="""Формат файла csv [name, price, min_price, fix_price, currency, category(не обязательное поле)]"""
 )
 async def add_domains(
     file: UploadFile = File(...)
@@ -80,18 +93,13 @@ async def add_domains(
                 'domain': row[0],
                 'forsale': 1,
                 'price': float(row[1]),
-                'minprice': 0,
-                'fixedprice': 1,
-                'currency': 1,
+                'minprice': float(row[2]),
+                'fixedprice': row[3],
+                'currency': row[4],
+                'category': list(row[5]) if len(row) > 5 else None,
                 'domainlanguage': 'en',
             }
-            domain_entry_xml = client.get_type('ns0:DomainEntry')(domain=domain_entry['domain'],
-                                                                  forsale=domain_entry['forsale'],
-                                                                  price=domain_entry['price'],
-                                                                  minprice=domain_entry['minprice'],
-                                                                  fixedprice=domain_entry['fixedprice'],
-                                                                  currency=domain_entry['currency'],
-                                                                  domainlanguage=domain_entry['domainlanguage'])
+            domain_entry_xml = client.get_type('ns0:DomainEntry')(**domain_entry)
             domain_entries.append(domain_entry_xml)
 
         chunked_entries = [domain_entries[i:i + 50] for i in range(0, len(domain_entries), 50)]
@@ -113,6 +121,69 @@ async def add_domains(
 
         return {'message': 'Запрос принят, асинхронное добавление доменов выполняется в фоне.'}
 
+    except KeyError as er:
+        logger.info(er)
+        return {'error': 'Ошибка в формате данных, проверьте правильность передаваемых данных'}
+
+    except Exception as er:
+        logger.info(er)
+        return {'error': 'Ошибка при добавлении доменов'}
+
+
+@router.put(
+    '/update_domains',
+    summary="Update domains from CSV file",
+    description="""Формат файла csv [name, price, min_price, fix_price, currency, category(не обязательное поле)]"""
+)
+async def update_domains(
+    file: UploadFile = File(...)
+):
+    try:
+        transport = Transport(timeout=10, session=requests.Session())
+        client = zeep.Client('https://api.sedo.com/api/v1/?wsdl', transport=transport)
+        domain_entries = []
+
+        contents = await file.read()
+        csv_data = StringIO(contents.decode())
+        df = pd.read_csv(csv_data, header=None)
+
+        for index, row in df.iterrows():
+            domain_entry = {
+                'domain': row[0],
+                'forsale': 1,
+                'price': float(row[1]),
+                'minprice': float(row[2]),
+                'fixedprice': row[3],
+                'currency': row[4],
+                'category': list(row[5]) if len(row) > 5 else None,
+                'domainlanguage': 'en',
+            }
+            domain_entry_xml = client.get_type('ns0:DomainEntry')(**domain_entry)
+            domain_entries.append(domain_entry_xml)
+
+        chunked_entries = [domain_entries[i:i + 50] for i in range(0, len(domain_entries), 50)]
+
+        tasks = []
+
+        for i, chunk in enumerate(chunked_entries, 1):
+            params = {
+                'partnerid': settings.partner_id,
+                'signkey': settings.sign_key,
+                'username': settings.api_login,
+                'password': settings.api_pass,
+                'domainentry': chunk,
+            }
+            task = asyncio.create_task(update_domain_async(client, params, i, len(chunked_entries)))
+            tasks.append(task)
+
+        asyncio.create_task(handle_async_tasks(tasks))
+
+        return {'message': 'Запрос принят, асинхронное изменение доменов выполняется в фоне.'}
+
+    except KeyError as er:
+        logger.info(er)
+        return {'error': 'Ошибка в формате данных, проверьте правильность передаваемых данных'}
+
     except Exception as er:
         logger.info(er)
         return {'error': 'Ошибка при добавлении доменов'}
@@ -120,7 +191,8 @@ async def add_domains(
 
 @router.delete(
     '/delete_domains',
-    summary="Delete domains from CSV file"
+    summary="Delete domains from CSV file",
+    description="""Формат файла csv [name]"""
 )
 async def delete_domains(
     file: UploadFile = File(...)
@@ -158,7 +230,8 @@ async def delete_domains(
 
 @router.get(
     '/domains_list/{file_name}',
-    summary="Get domains list to CSV file"
+    summary="Get domains list to CSV file",
+    description="""Формат файла csv [name]"""
 )
 async def domains_list(
     file_name: str
@@ -193,7 +266,8 @@ async def domains_list(
 
 @router.post(
     '/domains_status',
-    summary="Domains status from CSV file"
+    summary="Domains status from CSV file",
+    description="""Формат файла csv [name]"""
 )
 async def domains_status(
     file: UploadFile = File(...)
